ad500a169788eb984efe9896792bec7e421ce6b6
saipreetham <145094515+saipreethamsanda@users.noreply.github.com>
2024-05-17 09:51:40 +0000

    test

--- calculator.c
+++ calculator.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 int main()
 {
-   int num1 ,num2,result;
-   printf("Enter the numbers:  " " ");
+   int num1 ,num2,result,result1,result2;
+   printf("Enter the numbers: ");
    scanf("%d %d",&num1,&num2);
-   result = num1 - num2;
-   result1 = num1 * num2;
+   result = num1 + num2;
+   result1 = num1 * num2/num1;
    result2 = num1 / num2;
 
    printf("Sum of %d and %d is %d\n",num1 ,num2,result);
